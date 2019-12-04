e29c53d6eab2138601d95d0c96aa15d3cacb56e0
ruksanams <58158562+ruksanams@users.noreply.github.com>
2019-12-04 09:35:09 +0530

    encapsulation

---
+++ encapsulation.java
@@ -0,0 +1,40 @@
+package corejava;
+
+import java.util.Scanner;
+
+public class encapsulation {
+
+	String name,address,phone,email,prooftype,proofid;
+	 public static void main(String[]args)
+	  {
+		 Scanner obj=new Scanner(System.in);
+			System.out.println("Enter your Name:");
+			 corejava31 ob=new corejava31();
+		    String name=obj.nextLine();
+		    System.out.println("Enter your Address:");
+		    String address=obj.nextLine();
+		    System.out.println("Enter your Phone no:");
+		    String phone=obj.nextLine();
+		    System.out.println("Enter your Email id:");
+		    String email=obj.nextLine();
+		    System.out.println("Enter your Proof Type:");
+		    String prooftype=obj.nextLine();
+		    System.out.println("Enter your Proof id:");
+		    String proofid=obj.nextLine();
+		    ob.setname(name);
+		    ob.setaddress(address);
+		    ob.setphone(phone);
+		    ob.setemail(email);
+		    ob.setprooftype(prooftype);
+		    ob.setproofid(proofid);
+		    System.out.println("  Thank you for registration\n       Your details are");
+		    System.out.println("Name       : "+ob.getname());
+		    System.out.println("Address    : "+ob.getaddress());
+		    System.out.println("Email Id   : "+ob.getphone());
+		    System.out.println("Phone no   : "+ob.getemail());
+		    System.out.println("Proof Type : "+ob.getprooftype());
+		    System.out.println("Proof Id   : "+ob.getproofid());
+		    
+	  }
+}
+
---
+++ encapsulation1.java
@@ -0,0 +1,55 @@
+package corejava;
+
+public class encapsulation1 {
+
+	String name,address,phone,email,prooftype,proofid;
+	public void setname(String name)
+	{
+		this.name=name;
+	}
+	public void setaddress(String address)
+	{
+		this.address=address;
+	}
+	public void setphone(String phone)
+	{
+		this.phone=phone;
+	}
+	public void setemail(String email)
+	{
+		this.email=email;
+	}
+	public void setprooftype(String prooftype)
+	{
+		this.prooftype=prooftype;
+	}
+	public void setproofid(String proofid)
+	{
+		 this.proofid=proofid;
+	}
+    public String getname()
+    {
+    	return this.name;
+}
+    public String getaddress()
+    {
+    	return this.address;
+}
+    public String getphone()
+    {
+    	return this.phone;
+}
+    public String getemail()
+    {
+    	return this.email;
+}
+    public String getprooftype()
+    {
+    	return this.prooftype;
+}
+    public String getproofid()
+    {
+    	return this.proofid;
+}
+}
+
