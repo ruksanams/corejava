fbf54272d6302d99029962c77270183a54b67927
ruksanams <58158562+ruksanams@users.noreply.github.com>
2019-12-12 15:45:15 +0530

    Objectclass

--- Objectclass.java
+++ Objectclass.java
@@ -1,35 +1,33 @@
 package objclass;
 
-public class objectclass {
+public class Objectclass
+     {
 	static int last_roll;
 	int roll_no;
 	//constructor
 	objectclass()
-	{
+	   {
 		roll_no =last_roll;
 		last_roll++;
-		
-	}
-	public int hashcode()
-	{
+           }
+       public int hashcode()
+	   {
 		return roll_no;
-	}
+	   }
 	public static void main(String[]args)
-	{
+	   {
 		objectclass s=new objectclass();
 		System.out.println(s);
 		System.out.println(s.toString());
 		Object obj=new String("Face");
 		Class c=obj.getClass();
 		System.out.println("class of object obj is : "+c.getName());
-		
 		s=null;
 		System.gc();//garbage collection
-	}
-	
+	  }
 	protected void finalize()
-	{
+	  {
 		System.out.println("finalize metod called");
-	}
+	  }
 }
 
