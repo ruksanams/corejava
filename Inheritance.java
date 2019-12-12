515bb21f06fc18d453f294667c1f8fd80664d325
ruksanams <58158562+ruksanams@users.noreply.github.com>
2019-12-12 15:41:10 +0530

    Inheritance Example

--- Inheritance.java
+++ Inheritance.java
@@ -1,22 +1,23 @@
 package corejava;
 import java.util.Scanner;
-class A{
-	public void add(int a,int b)
-	{
-		int c=a+b;
-		System.out.print("sum is "+c);
-	}
-	
+class A
+{
+  public void add(int a,int b)
+   {
+     int c=a+b;
+     System.out.print("sum is "+c);
+   }
 }
-
-public class inheritance extends A{
+public class inheritance extends A//inherited class
+  {
 	int a,b;
-	public void add(int a,int b) {
-		int c=a+b+10;
+	public void add(int a,int b) //add method
+	   {
+	        int c=a+b+10;
 		System.out.print("sum is "+c);
-	}
+	   }
 	public static void main(String[]args)
-	{
+	   {
 		Scanner obj=new Scanner(System.in);
 		System.out.println("Enter the first number  : ");
 		int a=obj.nextInt();
@@ -24,6 +25,5 @@ public class inheritance extends A{
 		int b=obj.nextInt();
 		A ob=new inheritance();
 		ob.add(a,b);
-	}
-
-}
+	  }
+   }
