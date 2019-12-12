2be966550264f8eff5385eac8c8a013f76eccfb3
ruksanams <58158562+ruksanams@users.noreply.github.com>
2019-12-12 12:35:53 +0530

    Autoboxing

--- Autoboxing.java
+++ Autoboxing.java
@@ -1,11 +1,10 @@
 package Wrapper;
-
-public class Autoboxing {
-	public static void main(String[]args)
+public class Autoboxing 
+  {
+     public static void main(String[]args)
 	{
-		Integer i=20;//converting into integer
-		Integer j=i;//autoboxing, now compiler will write Integer.valueOf(a) internally
-		System.out.println(i+" "+j);
+           Integer i=20;//converting into integer
+	   Integer j=i;//autoboxing, now compiler will write Integer.valueOf(a) internally
+	   System.out.println(i+" "+j);
 	}
-
-}
+  }
