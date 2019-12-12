f7a2f5ad8fed1ea7485353093fc85f6693aa440e
ruksanams <58158562+ruksanams@users.noreply.github.com>
2019-12-12 12:38:10 +0530

    Unboxing

--- Unboxing.java
+++ Unboxing.java
@@ -1,13 +1,13 @@
 package Wrapper;
-
-public class Unboxing {
-public static void main(String args[])
+public class Unboxing
 {
+  public static void main(String args[])
+   {
 	//converting Integer to int
 	Integer a=new Integer(10);
 	int i=a.intValue();//converting Integer to int
 	int j=i;//unboxing now compiler will write a.inValue() internally
 	System.out.println(a+" "+i+" "+j);
-}
+   }
 }
 
