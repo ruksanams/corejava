01986e72967fba08b0df8884189fc04fd23d0c8f
ruksanams <58158562+ruksanams@users.noreply.github.com>
2019-12-12 12:57:34 +0530

    Registration using constructor

--- corejava2.java
+++ corejava2.java
@@ -1,32 +1,31 @@
 package corejava;
-
 import java.util.Scanner;
-
-public class corejava2 {
-	public String name,address,phone,email,prooftype,proofid;
-	public corejava2(String name,String address,String phone,String email,String prooftype,String proofid)
-	{
+public class Corejava2 
+    {
+	public String name,address,phone,email,prooftype,proofid;//data Members
+	public corejava2(String name,String address,String phone,String email,String prooftype,String proofid)//parameterised constructor
+	   {
 		this.name=name;
 		this.address=address;
 		this.phone=phone;
 		this.email=email;
 		this.prooftype=prooftype;
 		this.proofid=proofid;
-	}
-public void register()
-{
-	System.out.println("     Thank you for registration\n          Your details are");
-    System.out.println("Name       : "+name);
-    System.out.println("Address    : "+address);
-    System.out.println("Email Id   : "+phone);
-    System.out.println("Phone no   : "+email);
-    System.out.println("Proof Type : "+prooftype);
-    System.out.println("Proof Id   : "+proofid);
-}
-	public static void main(String[]args)
+	   }
+        public void register()//register method
+         {
+	  System.out.println("     Thank you for registration\n          Your details are");
+          System.out.println("Name       : "+name);
+          System.out.println("Address    : "+address);
+          System.out.println("Email Id   : "+phone);
+          System.out.println("Phone no   : "+email);
+          System.out.println("Proof Type : "+prooftype);
+          System.out.println("Proof Id   : "+proofid);
+         }
+	 public static void main(String[]args)
 	  {
-		Scanner obj=new Scanner(System.in);
-		System.out.println("Enter your Name:");
+	    Scanner obj=new Scanner(System.in);
+	    System.out.println("Enter your Name:");
 	    String name=obj.nextLine();
 	    System.out.println("Enter your Address:");
 	    String address=obj.nextLine();
@@ -38,7 +37,7 @@ public void register()
 	    String prooftype=obj.nextLine();
 	    System.out.println("Enter your Proof id:");
 	    String proofid=obj.nextLine();
-	    corejava2 ob=new corejava2(name,address,phone,email,prooftype,proofid);
-	    ob.register();
+	    Corejava2 ob=new Corejava2(name,address,phone,email,prooftype,proofid);//calling constructor
+	    ob.register();//calling method using object
 	  }
-}
+ }
