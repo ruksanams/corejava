1f93df6a7990d0f45360c5046e9c751afa9f1ddb
ruksanams <58158562+ruksanams@users.noreply.github.com>
2019-12-13 12:55:50 +0530

    Intern function

---
+++ MyClass.java
@@ -0,0 +1,15 @@
+package strings;
+public class MyClass 
+{
+  public static void main(String args[])
+  {
+	String a="well";
+	String ab="All is " +a;
+	String abc="All is well";
+	String abcd="All is well";
+	System.out.println(abc.hashCode());
+	System.out.println(ab.hashCode());
+	System.out.println(abc == ab.intern());//it will share the same memory
+	System.out.println(abcd.hashCode());
+  }
+}
