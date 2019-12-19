96228495848a54251f25e1d57f1920a47818b462
ruksanams <58158562+ruksanams@users.noreply.github.com>
2019-12-19 18:31:41 +0530

    Searching a particular string

---
+++ ContainsExample1.java
@@ -0,0 +1,11 @@
+package stringmethods;
+public class ContainsExample1
+{
+  public static void main(String args[])
+  {
+	  String name="what you know about me";
+	  System.out.println(name.contains("do you know"));//check the particular word present in the given string
+	  System.out.println(name.contains("about"));
+      System.out.println(name.contains("hello"));
+  }
+}
---
+++ ContainsExample3.java
@@ -0,0 +1,14 @@
+package stringmethods;
+public class ContainsExample3
+{
+  public static void main(String args[])
+  {
+	  String str="To learn java visit focusacademy.in";
+	  if(str.contains("focusacademy.in.com"))
+	    {
+		  System.out.println("This string contains focusacademy.in");
+	    }
+	  else
+		  System.out.println("Result not found");
+  }
+}
